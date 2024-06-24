e00ca71829003ff319fe6352489c56d3cb847714
sethriley <70654481+sethriley@users.noreply.github.com>
2024-06-24 14:47:48 -0500

    Add files via upload

---
+++ RenameFieldUsingSameField.py
@@ -0,0 +1,17 @@
+def calculatefield(TERM):
+    if TERM == "30-YRS":
+        return "30 Years"
+    if TERM == "20-YRS":
+        return "20 Years"
+    if TERM == "5-YRS":
+        return "5 Years"
+    if TERM == "50-YRS":
+        return "50 Years"
+    if TERM == "25-YRS":
+        return "25 Years"
+    if TERM == "10-YRS":
+        return "10 Years"
+    if TERM == "29-YRS & 4-MONTHS":
+        return "29 Years & 4 Months"
+    else:
+        return "MARK"
